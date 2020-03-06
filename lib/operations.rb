0d5bf79be31d46f4ed434bf5f356d4daa4f0d472
Christopher Cid <iamcid17@gmail.com>
2020-03-06 05:46:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,8 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
+    return true
+  else
+    return false
   end
 end
 
@@ -7,6 +10,10 @@ end
 
 def not_safe?(speed)
   not_safe ? speed < 40 : speed > 60
+  return true
+ else
+  return false
+ end
 end
 	
 
